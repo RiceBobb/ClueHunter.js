fcd4d959b472407a3fc479bc9ec23c67762bbdb5
minsing-jin <ironman0722@naver.com>
2025-01-08 23:05:41 +0900

    Complete bm25, index.ts, pacakge info files

--- src/search-engine/bm25.ts
+++ src/search-engine/bm25.ts
@@ -1,13 +1,17 @@
-const { BM25Retriever } = require("@langchain/community/retrievers/bm25");
+import {BM25Retriever} from "@langchain/community/retrievers/bm25";
+import { Document } from "@langchain/core/documents";
 
-export async function bm25_search(query: string, contents: string[]) {
+export async function bm25_search(query: string, contents: Document[], passage_limit = contents.length, showScore=true): Promise<Document<Record<string, any>>[]> {
+  if (!query || !contents) {
+    throw new Error("Query and contents must be provided");
+  }
+  
   const retriever = BM25Retriever.fromDocuments(
     contents,
-    { includeScore: true }
+    { k: passage_limit, includeScore: showScore }
   );
 
   const results = await retriever.invoke(query);
-  console.log(results);
 
   return results;
 }
