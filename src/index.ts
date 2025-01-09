42578914a49076cb79679a87146489f6db54a600
minsing-jin <ironman0722@naver.com>
2025-01-09 22:07:19 +0900

    Conflict resolve

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import { bm25_search } from "./search-engine/bm25.js";
+import { wink_splitter } from "./sentence-splitter/wink_splitter.js";
+import { rerank } from "./search-engine/rerank.js";
 
-export { bm25_search };
+export { bm25_search, wink_splitter };    
+
+export { rerank };
