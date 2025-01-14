877bd1c75200296fcc47eb276ce6d4aba67b1818
minsing-jin <ironman0722@naver.com>
2025-01-14 17:00:01 +0900

    Add no sentence retrieved case and refactor clueHunt func to class

--- src/index.ts
+++ src/index.ts
@@ -6,30 +6,41 @@ function convert_doc_to_arr(results: any[]): string[] {
   return results.map((result) => result.pageContent);
 }
 
-export async function clueHunt(
-  answer: string,
-  parsed_text: string,
-  bm25_top_k: number = 50,
-  device: "cpu" | "webgpu" = "cpu",
-) {
+export class ClueHunter {
+  private device: "cpu" | "webgpu";
+  private bm25_top_k: number;
 
-  const passages = sbd_splitter(parsed_text);
+  constructor(device: "cpu" | "webgpu" = "cpu", bm25_top_k: number = 50) {
+    this.device = device;
+    this.bm25_top_k = bm25_top_k;
+  }
 
-  const searched_passages_doc = await bm25_search(
-    answer,
-    passages,
-    bm25_top_k
-  );
+  async clueHunt(answer: string, parsed_text: string): Promise<string> {
+    const passages = sbd_splitter(parsed_text);
 
-  const searched_passages_arr = convert_doc_to_arr(searched_passages_doc);
+    const searched_passages_doc = await bm25_search(
+      answer,
+      passages,
+      this.bm25_top_k
+    );
 
-  const reranked_results = await rerank(answer, searched_passages_arr, {
-    top_k: 1,
-    return_documents: true,
-    device: device,
-  });
+    const searched_passages_arr = convert_doc_to_arr(searched_passages_doc);
 
-  return reranked_results[0].text;
+    const reranked_results = await rerank(answer, searched_passages_arr, {
+      top_k: 1,
+      return_documents: true,
+      device: this.device,
+    });
+
+
+    if (reranked_results.length > 0 && reranked_results[0].text) {
+      return reranked_results[0].text;
+    } else {
+      console.warn("No answer found");
+      console.log(`Top-0 sentence: ${reranked_results[0].score}`);
+      return "No answer found";
+    }
+  }
 }
 
 export { bm25_search, sbd_splitter, rerank };
