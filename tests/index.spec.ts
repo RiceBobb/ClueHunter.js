fb0e21e47448e954084f75b65eed28f03569cd6a
kimbwook <bwook00@naver.com>
2025-01-14 16:22:26 +0900

    Increase timeout for ClueHunt test to improve reliability

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -60,7 +60,8 @@ function wait(ms: number) {
           
     
 describe("ClueHunt 50 Test", async () => {
-  it("should find relevant sentences", async () => {
+  it("should find relevant sentences", async function() {
+    this.timeout(10000);
 
     const results = await clueHunt(testQuery, testDocuments, 50, 'cpu', [2, 3]);
 
