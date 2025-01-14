b3816359ccf5b12b013e37d3c991990a02e990c5
minsing-jin <ironman0722@naver.com>
2025-01-14 22:09:02 +0900

    Add annotation and newline_boundaries param

--- src/sentence-splitter/sbd_splitter.ts
+++ src/sentence-splitter/sbd_splitter.ts
@@ -4,12 +4,12 @@ import tokenizer from "sbd";
   Sentence Boundary Detection (SBD) splitter
 
   Optional_options parameter:
-  - newline_boundaries, force sentence split at newlines
-  - html_boundaries, force sentence split at specific tags (br, and closing p, div, ul, ol)
-  - sanitize: If you don't expect nor want html in your text.
-  - allowed_tags: To sanitize html, the library santize-html is used. You can pass the allowed tags option.
-  - preserve_whitespace: Preserve the literal whitespace between words and sentences (otherwise, internal spaces are normalized to a single space char, and inter-sentence whitespace is omitted). Preserve whitespace has no effect if either newline_boundaries or html_boundaries is specified.
-  - abbreviations: list of abbreviations to override the original ones for use with other languages. Don't put dots in your custom abbreviations.
+  - newline_boundaries: boolean | undefined (force sentence split at newlines)
+  - html_boundaries: boolean | undefined (force sentence split at specific tags (br, and closing p, div, ul, ol))
+  - sanitize: boolean | undefined (If you don't expect nor want html in your text.)
+  - allowed_tags: false | string[] | undefined (To sanitize html, the library santize-html is used. You can pass the allowed tags option.)
+  - preserve_whitespace: boolean | undefined (Preserve the literal whitespace between words and sentences (otherwise, internal spaces are normalized to a single space char, and inter-sentence whitespace is omitted). Preserve whitespace has no effect if either newline_boundaries or html_boundaries is specified.)
+  - abbreviations: string[] | undefined (list of abbreviations to override the original ones for use with other languages. Don't put dots in your custom abbreviations.)
  */
 
 export function sbd_splitter(parsed_text: string): string[] {
@@ -17,7 +17,7 @@ export function sbd_splitter(parsed_text: string): string[] {
     throw new Error("Parsed text and contents must be provided");
   }
 
-  var optional_options = {};
+  var optional_options = { newline_boundaries: true };
   var passages = tokenizer.sentences(parsed_text, optional_options);
 
   return passages;
